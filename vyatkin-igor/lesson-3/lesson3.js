d45866a4c7c2288bbce8c3eb2575c80a80b971f6
Gareei <67067868+Gareei@users.noreply.github.com>
2020-07-14 21:33:28 +0500

    Update lesson3.js
    Массив для хранения товаров в корзине;
    Функция countBasketPrice, которая считает стоимость корзины.

--- vyatkin-igor/lesson-3/lesson3.js
+++ vyatkin-igor/lesson-3/lesson3.js
@@ -22,3 +22,42 @@ while (i <= n) {
 }
 
 /*------------------ end part 1 --------------------------*/
+
+
+/*------------------ begin part 2,3 --------------------------*/
+const shop = [
+    ["CPU AMD Ryzen 5 Pinnacle Ridge", 11990, "RUB", 2],
+    ["MB GIGABYTE B450M DS3H", 5390, "RUB", 2],
+    ["GPU SAPPHIRE AMD Radeon RX 580", 17990, "RUB", 2],
+    ["RAM CRUCIAL CT8G4DFS8266 DDR4 - 8Gb", 2590, "RUB", 2],
+    ["БП SEASONIC FOCUS Plus SSR-550PX", 9750, "RUB", 2],
+    ["SSD KINGSTON A400 SA400S37", 2590, "RUB", 2],
+    ["Monitor SAMSUNG C24RG50FQI 23.5", 13990, "RUB", 2],
+    ["Midi-Tower ATX ZALMAN S2", 2430, "RUB", 2]
+
+];
+
+function countBasketPrice(items) {
+    let ammount = 0;
+    //TODO - расчет стоимости
+    let price;
+    let count;
+    let cost_positions;
+    for (let i = 0; i < shop.length; i++) {
+        for (let j = 1; j < shop[i].length; j = j + 2) {
+            if (j == 1) price = shop[i][j];
+            if (j == 3) {
+                count = shop[i][j];
+                cost_positions = price * count;
+            }
+        }
+        ammount += cost_positions;
+    }
+
+    return ammount;
+}
+
+console.log('Общая стоимость товаров: ', countBasketPrice(shop));
+
+
+/*------------------ end part 2,3 --------------------------*/
