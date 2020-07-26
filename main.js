185e275ab739aabedab9292e6d5ca6857b56b329
ArtemGrebenyuk <38192442+ArtemGrebenyuk@users.noreply.github.com>
2020-07-26 15:44:22 +0300

    Add task2 from lesson 5
    Не смог разобраться как переиспользовать код для addBtn. Нужно как-то определять какая именно кнопка товара нажата, не придумал ничего лучше чем написать каждой кнопке уникальный id и определять кнопку по id, описывая разными функциями. Возможно подскажете как описать одной функцией по единому класу для всех кнопок но при этом передавать вместе с нажатием кнопки какое-то свойство чтобы ее отличать и обрабатывать по своему.

--- main.js
+++ main.js
@@ -1,42 +1,86 @@
-        window.onload = function(){
-            let chess = document.getElementById('chess');
-            let Alfabet = ['A','B','C','D','E','F','G','H'];
+const basket__parent = document.querySelector('.basket__parent');
+const addBtn1 = document.querySelector('#item1');
+const addBtn2 = document.querySelector('#item2');
+const addBtn3 = document.querySelector('#item3');
+const resetBtn = document.querySelector('.reset');
+const basket = document.querySelector('.basket__parent');
+const text = document.querySelector('#default__text');
+const summary = document.createElement('div');
+summary.className = 'summary';
+
+class Basket{
+    constructor(nameitem, price, quantity){
+    this.name = nameitem;
+    this.price = price;
+    this.quantity = quantity;
+    }
+    show(){
+    console.log(this.name, this.price, this.quantity);
+    }
+}
+
+let sum = 0;
+
+const item1 = new Basket('chair', 100, 2);
+const item2 = new Basket('table', 400, 1);
+const item3 = new Basket('lamp', 200, 1);
 
 
-            for (let i=0; i<9; i++)
-            {
-                let row = document.createElement('div');
-                row.className = 'row';
+addBtn1.addEventListener('click', function(){
+    let basket__item = document.createElement('div');
+    basket__item.className = 'basket__item';
+    basket__item.textContent = item1.name + " " + item1.price;
 
-                for (let j=0; j<9; j++){
-                let cell = document.createElement('div');
-                cell.className = 'cell';
-                
-                if (i == 0 && j > 0) {
-                    cell.innerText = Alfabet[j-1];
-                    row.appendChild(cell);
-                    continue;
-                }
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item1.price;
+    summary.textContent = "На сумму " + sum;;
+    basket.appendChild(summary);
+    return (sum);
+});
 
-                if (i > 0 && j == 0) {
-                    cell.innerText = i;
-                    row.appendChild(cell);
-                    continue;
-                }
-                
-                if (i==0 && j == 0) {
-                    row.appendChild(cell);
-                    continue;
-                }
-                else if ((j - i) % 2) {
-                    cell.className = 'cell black';
-                    }
-                    else {
-                    cell.className = 'cell';
-                    }
-                row.appendChild(cell);
-                }
-            chess.appendChild(row);
-            }
-          
-        }
+addBtn2.addEventListener('click', function(){
+    let basket__item = document.createElement('div');
+    basket__item.className = 'basket__item';
+    basket__item.textContent = item2.name + " " + item2.price;
+    basket.appendChild(basket__item);
+
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item2.price;
+    summary.textContent = "На сумму " + sum;;
+    basket.appendChild(summary);
+    return (sum);
+});
+
+addBtn3.addEventListener('click', function(){
+    let basket__item = document.createElement('div');
+    basket__item.className = 'basket__item';
+    basket__item.textContent = item3.name + " " + item2.price;
+    basket.appendChild(basket__item);
+
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item3.price;
+    summary.textContent = "На сумму " + sum;;
+    basket.appendChild(summary);
+    return (sum);
+});
