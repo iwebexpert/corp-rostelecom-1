74c54bb4cd4b430d18a5472208835768ffdcfa65
ArtemGrebenyuk <38192442+ArtemGrebenyuk@users.noreply.github.com>
2020-07-27 23:07:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,11 @@ const basket__parent = document.querySelector('.basket__parent');
 const addBtn1 = document.querySelector('#item1');
 const addBtn2 = document.querySelector('#item2');
 const addBtn3 = document.querySelector('#item3');
-const addBtn = document.querySelectorAll('.add');
 const resetBtn = document.querySelector('.reset');
 const basket = document.querySelector('.basket__parent');
 const text = document.querySelector('#default__text');
 const summary = document.createElement('div');
-
+summary.className = 'summary';
 
 class Basket{
     constructor(nameitem, price, quantity){
@@ -21,27 +20,67 @@ class Basket{
 }
 
 let sum = 0;
-const item1 = new Basket('table', 400, 1);
-const item2 = new Basket('chair', 100, 2);
+
+const item1 = new Basket('chair', 100, 2);
+const item2 = new Basket('table', 400, 1);
 const item3 = new Basket('lamp', 200, 1);
-basketMas = [item1, item2, item3];
 
 
-function clkfunc(event){
+addBtn1.addEventListener('click', function(){
     let basket__item = document.createElement('div');
     basket__item.className = 'basket__item';
-    basket__item.textContent = basketMas[this.dataset.item].name + ' ' + basketMas[this.dataset.item].price;
-    text.innerHTML = "В корзине товары:";
-    basket.appendChild(basket__item);
-    sum += basketMas[this.dataset.item].price;
-    summary.innerHTML = "На сумму " + sum;
-    summary.className = 'summary';
+    basket__item.textContent = item1.name + " " + item1.price;
+
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item1.price;
+    summary.textContent = "На сумму " + sum;;
     basket.appendChild(summary);
-}
+    return (sum);
+});
 
-for (let i=0; i<3; i++){
-    addBtn[i].addEventListener('click', clkfunc);
-    //console.log(addBtn[2]);
-}
+addBtn2.addEventListener('click', function(){
+    let basket__item = document.createElement('div');
+    basket__item.className = 'basket__item';
+    basket__item.textContent = item2.name + " " + item2.price;
+    basket.appendChild(basket__item);
 
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item2.price;
+    summary.textContent = "На сумму " + sum;;
+    basket.appendChild(summary);
+    return (sum);
+});
 
+addBtn3.addEventListener('click', function(){
+    let basket__item = document.createElement('div');
+    basket__item.className = 'basket__item';
+    basket__item.textContent = item3.name + " " + item2.price;
+    basket.appendChild(basket__item);
+
+    if (text.textContent == 'Корзина пуста'){
+        text.remove();
+        text.textContent = "В корзине товары:";
+        console.log(text);
+        basket.appendChild(text);
+    }
+    
+    basket.appendChild(basket__item);    
+    sum += item3.price;
+    summary.textContent = "На сумму " + sum;;
+    basket.appendChild(summary);
+    return (sum);
+});
