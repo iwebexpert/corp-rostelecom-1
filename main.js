2f2502fea9acaaef9ea7e81ee31eacecd9a8a240
ArtemGrebenyuk <38192442+ArtemGrebenyuk@users.noreply.github.com>
2020-08-02 17:25:36 +0300

    Домашнее задание 7. Не понял как передавать значение переменной в новую страницу.
    Не разобрался как передать значение переменной со страницы покупок в новую страницу корзины, поэтому реализовал все на одной странице скрывая элементы. В задании именно создать новую страницу, но это еще не обсуждалось не в одной теме.

--- main.js
+++ main.js
@@ -1,86 +1,135 @@
-const basket__parent = document.querySelector('.basket__parent');
 const addBtn1 = document.querySelector('#item1');
 const addBtn2 = document.querySelector('#item2');
 const addBtn3 = document.querySelector('#item3');
+const addBtn = document.querySelectorAll('.add');
 const resetBtn = document.querySelector('.reset');
 const basket = document.querySelector('.basket__parent');
 const text = document.querySelector('#default__text');
-const summary = document.createElement('div');
-summary.className = 'summary';
+const background = document.querySelector('.backgroud')
+const summary = document.querySelector('.summary');
+const basketBody = document.querySelector('.basket');
+const basketBtn = document.querySelector('#basketBtn');
+const backBtn = document.createElement('button');
+const mainPage = document.querySelector('.main__page');
+const basketContainer = document.querySelector('.basket__container')
+let sum = 0;
+
 
 class Basket{
-    constructor(nameitem, price, quantity){
+    constructor(nameitem, price){
     this.name = nameitem;
     this.price = price;
-    this.quantity = quantity;
+    this.quantity = 0;
     }
-    show(){
-    console.log(this.name, this.price, this.quantity);
+    add(){
+        this.quantity += 1;
+    }
+    remove(){
+        if (this.quantity > 0) {
+            this.quantity -= 1;
+        }
     }
 }
 
-let sum = 0;
+const item1 = new Basket('table', 400);
+const item2 = new Basket('chair', 100);
+const item3 = new Basket('lamp', 200);
+basketMas = [item1, item2, item3];   
 
-const item1 = new Basket('chair', 100, 2);
-const item2 = new Basket('table', 400, 1);
-const item3 = new Basket('lamp', 200, 1);
-
-
-addBtn1.addEventListener('click', function(){
+function clkBuy(){
     let basket__item = document.createElement('div');
     basket__item.className = 'basket__item';
-    basket__item.textContent = item1.name + " " + item1.price;
+    basketMas[this.dataset.item].add();
+    sum += basketMas[this.dataset.item].price;
+    getSum(sum);
 
-    if (text.textContent == 'Корзина пуста'){
-        text.remove();
-        text.textContent = "В корзине товары:";
-        console.log(text);
-        basket.appendChild(text);
+}
+
+//рендеринг страницы Корзины
+function basketPage(basketMas) {
+    mainPage.className = 'hide';
+    basketContainer.innerHTML = '';
+    for (let i = 0; i < basketMas.length; i++)
+    {
+        if (basketMas[i].quantity > 0) {
+            renderItem(basketMas[i], i);
+        }
     }
-    
-    basket.appendChild(basket__item);    
-    sum += item1.price;
-    summary.textContent = "На сумму " + sum;;
-    basket.appendChild(summary);
-    return (sum);
-});
+    backBtn.innerHTML = "Закрыть корзину";
+    backBtn.addEventListener('click', function(){
+        mainPage.className = 'main__page';
+        basketContainer.innerHTML = '';
 
-addBtn2.addEventListener('click', function(){
+    });
+    basketContainer.appendChild(backBtn);
+
+}
+
+//addItem
+function addItem(item) {
+    item.add();
+    basketPage(basketMas);
+    sum += item.price;
+    getSum(sum);
+}
+
+//remItem
+function remItem(item) {
+    item.remove();
+    basketPage(basketMas);
+    sum -= item.price;
+    getSum(sum);
+}
+
+//update summary
+function getSum(sum) {
+    if (sum) {
+        summary.innerHTML = "В корзине товаров на сумму " + sum;
+    }
+    else {
+        summary.innerHTML = 'В корзине пусто';
+    }
+}
+
+//render basket__item
+function renderItem(item, i) {
     let basket__item = document.createElement('div');
     basket__item.className = 'basket__item';
-    basket__item.textContent = item2.name + " " + item2.price;
-    basket.appendChild(basket__item);
+    basket__item.id = i;
+    basket__item.innerHTML = 'наименование: ' + item.name + ' цена: ' + item.price + ' количество: ' + item.quantity
+    basketContainer.appendChild(basket__item);
+    let basketAdd = document.createElement('button');
+    basketAdd.className = 'basket__add';
+    basketAdd.innerHTML = '+';
+    basket__item.appendChild(basketAdd);
+    basketAdd.addEventListener('click', function(){addItem(item)});
+    let basketRem = document.createElement('button');
+    basketRem.className = 'basket__remove';
+    basketRem.innerHTML = '-';
+    basket__item.appendChild(basketRem);
+    basketRem.addEventListener('click', function(){remItem(item)});
+} 
 
-    if (text.textContent == 'Корзина пуста'){
-        text.remove();
-        text.textContent = "В корзине товары:";
-        console.log(text);
-        basket.appendChild(text);
+function handlers() {
+    for (let i=0; i<3; i++){
+        if (document.querySelector('.add')) {
+            addBtn[i].addEventListener('click', clkBuy);
+        }
     }
     
-    basket.appendChild(basket__item);    
-    sum += item2.price;
-    summary.textContent = "На сумму " + sum;;
-    basket.appendChild(summary);
-    return (sum);
-});
+    basketBtn.addEventListener('click', function() {basketPage(basketMas)});
+}
 
-addBtn3.addEventListener('click', function(){
-    let basket__item = document.createElement('div');
-    basket__item.className = 'basket__item';
-    basket__item.textContent = item3.name + " " + item2.price;
-    basket.appendChild(basket__item);
+function init() {
+    getSum(sum);
+    handlers();
+}
 
-    if (text.textContent == 'Корзина пуста'){
-        text.remove();
-        text.textContent = "В корзине товары:";
-        console.log(text);
-        basket.appendChild(text);
-    }
-    
-    basket.appendChild(basket__item);    
-    sum += item3.price;
-    summary.textContent = "На сумму " + sum;;
-    basket.appendChild(summary);
-    return (sum);
-});
+
+init();
+
+
+
+
+
+
