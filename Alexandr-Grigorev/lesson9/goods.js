794f7b62e2b9ff4117c555818c8ae16ca2a2545a
alexandr-grigorev <alexlipss@gmail.com>
2020-08-14 20:50:43 +0300

    Lesson 9

---
+++ Alexandr-Grigorev/lesson9/goods.js
@@ -0,0 +1,39 @@
+
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p><button class="addBtn">BUY</button></div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+            { title: 'Skirt', price: 200 },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
