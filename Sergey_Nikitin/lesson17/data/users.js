bbbad6b452b274a46410ad0290f4a4ee2792ce2b
Serg <serg_murmashi@mail.ru>
2020-09-03 19:32:37 +0300

    lesson 17 homework
    добавлена авторизация в предыдущее задание.

---
+++ Sergey_Nikitin/lesson17/data/users.js
@@ -0,0 +1,14 @@
+module.exports = {
+    anna: {
+        username: 'Anna',
+        age: 27,
+        skills: [
+            'JS',
+            'Node.js',
+            'Express.js'
+        ],
+    },
+    ivan: {
+        username: 'Ivan',
+    },
+}
