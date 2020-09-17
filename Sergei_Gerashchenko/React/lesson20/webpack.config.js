4b2437c0dcd9a905193632d7eb8b394567b1cd42
svg30 <gerik@live.ru>
2020-09-17 22:32:54 +0400

    Homework21

--- Sergei_Gerashchenko/React/lesson20/webpack.config.js
+++ Sergei_Gerashchenko/React/lesson20/webpack.config.js
@@ -7,18 +7,26 @@ module.exports = {
         path : path.join(__dirname, "dist"),
         filename: "bundle.js"
     },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.join(__dirname, 'src', 'components'),
+        },
+    },
+
     module : {
         rules: [
             // the 'transform-runtime' plugin tells Babel to
             // require the runtime instead of inlining it.
             {
-                test: /\.m?js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react' ],
-//                        plugins: ['@babel/plugin-transform-runtime']
+                        plugins: ['@babel/plugin-proposal-class-properties'],
                     }
                 }
             }
