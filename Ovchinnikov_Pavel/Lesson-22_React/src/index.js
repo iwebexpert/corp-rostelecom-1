62050f9236cc4c1617ac35833ecbcb1cf5e082d9
OPavlo <68392723+OPavlo@users.noreply.github.com>
2020-09-21 11:11:44 +0300

    Create index.js

---
+++ Ovchinnikov_Pavel/Lesson-22_React/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import { Layout } from 'components/Layout';
+
+ReactDom.render(
+    <Layout />,
+    document.getElementById('root')
+);
