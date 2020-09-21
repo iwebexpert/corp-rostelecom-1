9ea507586a9035b504f8989c56d9d5f9c9c9f6c8
svg30 <gerik@live.ru>
2020-09-21 23:00:40 +0400

    Homework 22

---
+++ Sergei_Gerashchenko/React/lesson22/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+import './Hader.css';
+
+export class Header extends Component {
+    render(){
+        return (<div className="logo">Messanger app</div>
+        );
+    }
+}
