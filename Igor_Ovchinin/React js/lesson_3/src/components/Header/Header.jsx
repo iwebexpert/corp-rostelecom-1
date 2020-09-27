c02e3a7e245d38519aaa928ce381db7540622bbc
Igor <ovchinin34@gmail.com>
2020-09-27 12:39:31 +0300

    React.js ДЗ №3

---
+++ Igor_Ovchinin/React js/lesson_3/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+
+import './Header.css';
+
+export class Header extends Component {
+    render() {
+        return (
+            <Grid
+                item xs={12}
+                className="logo"
+            >
+                The messenger
+            </Grid>
+        );
+    }
+}
