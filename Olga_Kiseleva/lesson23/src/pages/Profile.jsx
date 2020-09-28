d009e24885b5f7a3415fe72aa410b00425461f64
okis1986 <68300082+okis1986@users.noreply.github.com>
2020-09-28 20:13:40 +0300

    Create Profile.jsx

---
+++ Olga_Kiseleva/lesson23/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Grid, Container, List, ListItem, ListItemText } from '@material-ui/core';
+
+import { Profile } from 'components/Profile';
+
+export function ProfilePage() {
+    return (
+
+        <Profile />
+
+    );
+}
